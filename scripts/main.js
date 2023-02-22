c20e952a8ed9c7d4e0814ddb33d54475674cb4a2
Eugene Noble <71329328+Eugenenoble2005@users.noreply.github.com>
2023-02-22 22:38:16 +0100

    new commit

--- scripts/main.js
+++ scripts/main.js
@@ -15,51 +15,67 @@ function main()
     }
     player_colour = player_colour.split("")[0]
     //generate FEN string from board,
-    let fen_string = ""
-    for(var i =8;i>=1;i--){
-        for(var j=1;j<=8;j++){
-            let position = `${j}${i}`
-            //for every new row on the chessboard
-            if(j == 1 && i != 8){
-                fen_string+= "/"
-            }
-            let piece_in_position = document.querySelectorAll(`.piece.square-${position}`)[0]?.classList ?? null
-            //get piece name by shortest class
-            if(piece_in_position != null){
-                for(var item of piece_in_position.values()){
-                    if(item.length == 2){
-                        piece_in_position = item
+    function getFenString()
+    {
+        let fen_string = ""
+        for(var i =8;i>=1;i--){
+            for(var j=1;j<=8;j++){
+                let position = `${j}${i}`
+                //for every new row on the chessboard
+                if(j == 1 && i != 8){
+                    fen_string+= "/"
+                }
+                let piece_in_position = document.querySelectorAll(`.piece.square-${position}`)[0]?.classList ?? null
+                //get piece name by shortest class
+                if(piece_in_position != null){
+                    for(var item of piece_in_position.values()){
+                        if(item.length == 2){
+                            piece_in_position = item
+                        }
                     }
                 }
-            }
-            console.log(piece_in_position)
-            //if position is empty
-            if(piece_in_position == null){
-                //if previous position is empty, sum up numbers
-                let previous_char = fen_string.split("").pop()
-                if(!isNaN(Number(previous_char))){
-                    fen_string = fen_string.substring(0,fen_string.length-1)
-                    fen_string += Number(previous_char)+1
+                //if position is empty
+                if(piece_in_position == null){
+                    //if previous position is empty, sum up numbers
+                    let previous_char = fen_string.split("").pop()
+                    if(!isNaN(Number(previous_char))){
+                        fen_string = fen_string.substring(0,fen_string.length-1)
+                        fen_string += Number(previous_char)+1
+                    }
+                    else{
+                        fen_string+="1"
+                    }
                 }
-                else{
-                    fen_string+="1"
+                else if(piece_in_position?.split("")[0] == "b"){
+                    fen_string+=piece_in_position.split("")[1]
                 }
+                else if(piece_in_position?.split("")[0] == "w"){
+                    fen_string+=piece_in_position.split("")[1].toUpperCase()
+                }
+            
             }
-            else if(piece_in_position?.split("")[0] == "b"){
-                fen_string+=piece_in_position.split("")[1]
-            }
-            else if(piece_in_position?.split("")[0] == "w"){
-                fen_string+=piece_in_position.split("")[1].toUpperCase()
-            }
-        
         }
+        return fen_string
     }
+    let fen_string = getFenString()
     fen_string += ` ${player_colour}`
     console.log(fen_string)
     const engine = new Worker("/bundles/app/js/vendor/jschessengine/stockfish.asm.1abfa10c.js")
     engine.postMessage(`position fen ${fen_string}`)
     engine.postMessage('go wtime 300000 btime 300000 winc 2000 binc 2000');
     engine.postMessage("go debth 30")
+    step = 
+    setInterval(()=>{
+        let new_fen_string = getFenString()
+        new_fen_string += ` ${player_colour}`
+        if(new_fen_string != fen_string){
+            step = !step
+            fen_string = new_fen_string
+            engine.postMessage(`position fen ${fen_string}`)
+            engine.postMessage('go wtime 300000 btime 300000 winc 2000 binc 2000');
+            engine.postMessage("go debth 30")
+        }
+    })
     engine.onmessage = function(event){
         if (event.data.startsWith('bestmove')) {
             const bestMove = event.data.split(' ')[1];
@@ -68,25 +84,6 @@ function main()
           }
     }
     //listen for new moves
-    const targetNode = document.querySelector(".piece")
-    const config = { attributes: true, childList: true, subtree: true };
-    // Callback function to execute when mutations are observed
-    const callback = (mutationList, observer) => {
-    for (const mutation of mutationList) {
-        if (mutation.type === 'childList') {
-        console.log('A child node has been added or removed.');
-        } else if (mutation.type === 'attributes') {
-        console.log(`The ${mutation.attributeName} attribute was modified.`);
-        }
-    }
-    };
-
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
-
-    // Start observing the target node for configured mutations
-    observer.observe(targetNode, config);
-    observer.disconnect();
     return {status:true}
     
 }
